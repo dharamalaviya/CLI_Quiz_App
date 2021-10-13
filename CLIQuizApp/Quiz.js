ff73c3291888ca950b9d6ff8e9dbdbf52bcda2f6
dharamalaviya <you@example.com>
2021-10-13 18:53:29 +0530

    CLI Quiz App in Node.js

---
+++ CLIQuizApp/Quiz.js
@@ -0,0 +1,77 @@
+const question = require("./Quiz_QueAns");
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+const chalk = require("chalk");
+let blueBright = chalk.bold.blueBright
+let green = chalk.bold.green
+let red = chalk.bold.red
+let cyan = chalk.bold.cyan
+let yellow = chalk.bold.yellow
+let title = chalk.black.bold.yellow
+
+var answer = ""; 
+var qNo = 1;
+var score = 0;
+var username = "";
+var notes = "";
+
+var getQuestions = () =>{
+    var data = question.filter((o) => o.no == qNo);
+    data.forEach(nq => {
+        console.log(cyan(`\nNo : ${nq.no}`));
+        console.log(yellow(`Question : ${nq.q}`));
+        console.log(cyan(`\na : ${nq.a}`));
+        console.log(cyan(`b : ${nq.b}`));
+        console.log(cyan(`c : ${nq.c}`));
+        console.log(cyan(`d : ${nq.d}`));
+        answer = nq.ans;
+        notes = nq.note;
+    })
+};
+
+var checkAnswer = () => {
+    if(qNo <= 10){
+        rl.question(title("\nGive Answer : "),(ans) => {
+            if(ans == "a" || ans == "b" || ans == "c" || ans == "d"){
+                if(ans == answer){
+                    score += 5;
+                    console.log(green("\nRight Answer : " + ans));
+                    console.log(blueBright("Your Score : " + score));
+                    qNo += 1;
+                    repeat();
+                }else{
+                    score -= 2;
+                    console.log(red("\nWrong Answer : " + ans));
+                    console.log(blueBright("Your Score : " + score));
+                    console.log(green("\nDiscriptions: " + notes));
+                    qNo += 1;
+                    repeat();
+                }
+            }else{
+                console.log(red("\nWrong Choice, Please try again"));
+                repeat();
+            }
+        });
+    }else{
+        console.log(title("\nComplete Quiz..."));
+        console.log(title(`\nUserName : ${username}  your final score is : ${score}`));
+        rl.close();
+    }
+};
+
+var repeat = () => {
+    getQuestions();
+    checkAnswer();
+};
+
+console.log(title("Welcome to OOPs Quiz..!"));
+rl.question(title("\nPlease Enter Your Name : "),(ans) => {
+    username = ans;
+    repeat();
+});
---
+++ CLIQuizApp/Quiz_QueAns.js
@@ -0,0 +1,104 @@
+const Questions = [
+    {
+        no : 1,
+        q : "Which of the following language was developed as the first purely object programming language?",
+        a : "SmallTalk",
+        b : "C++",
+        c : "Kotlin",
+        d : "Java",
+        ans : "a",
+        note : "This programming language was invented as the first pure OOPS (object-oriented) language. This language was designed by Alan Kay in the early 1970s."
+    },
+    {
+        no : 2,
+        q : "Who developed object-oriented programming?",
+        a : "Adele Goldberg",
+        b : "Dennis Ritchie",
+        c : "Alan Kay",
+        d : "Andrea Ferro",
+        ans : "c",
+        note : "In the year 1970, Alan Kay gave Object-Oriented programming. He coined the concept of OOPS at a grad school in the year 1966 or 1967. Alan kay, Adele Goldberg, Dan Ingalls and others developed the first Smalltalk programming language, which follows the OOPS concept."
+    },
+    {
+        no : 3,
+        q : "Which of the following is not an OOPS concept?",
+        a : "Encapsulation",
+        b : "Polymorphism",
+        c : "Exception",
+        d : "Abstraction",
+        ans : "c",
+        note : "None."
+    },
+    {
+        no : 4,
+        q : "Which feature of OOPS described the reusability of code?",
+        a : "Abstraction",
+        b : "Encapsulation",
+        c : "Polymorphism",
+        d : "Inheritance",
+        ans : "d",
+        note : "Inheritance is the feature of OOPS, which allows the users of OOPS to reuse the code which is already written. This OOPS feature inherits the features of another class in the programs. This mechanism actually inherits the fields and methods of the superclass."
+    },
+    {
+        no : 5,
+        q : "Which of the following language supports polymorphism but not the classes?",
+        a : "C++ programming language",
+        b : "Java programming language",
+        c : "Ada programming language",
+        d : "C# programming language",
+        ans : "c",
+        note : "It is a programming language that disapproves of the concept of polymorphism but supports the concept of classes. It is an object-based language. So, it does not follow the Object-oriented programming concepts."
+    },
+    {
+        no : 6,
+        q : "Which among the following feature is not in the general definition of OOPS?",
+        a : "Modularity",
+        b : "Efficient Code",
+        c : "Code reusability",
+        d : "Duplicate or Redundant Data",
+        ans : "d",
+        note : "Duplicacy or Redundancy of data is a feature which totally dependent on the programmers. So, it cannot be created by the OOPS."
+    },
+    {
+        no : 7,
+        q : "Which feature of OOPS derives the class from another class?",
+        a : "Inheritance",
+        b : "Data hiding",
+        c : "Encapsulation",
+        d : "Polymorphism",
+        ans : "a",
+        note : "Inheritance is an important OOPS feature which derives the class from the base class or superclass. This OOPS feature inherits the features of another class in the programs. This mechanism actually inherits the fields and methods of the superclass."
+    },
+    {
+        no : 8,
+        q : "Define the programming language, which does not support all four types of inheritance?",
+        a : "Smalltalk",
+        b : "Kotlin",
+        c : "Java",
+        d : "C++",
+        ans : "c",
+        note : "Java is a programming language that disapproves of the concept of 'multiple inheritance'. So, it does not agree with all types of inheritance. But, we can implement 'multiple inheritance' in Java language using the interface concept."
+    },
+    {
+        no : 9,
+        q : "A single program of OOPS contains _______ classes?",
+        a : "Only 1",
+        b : "Only 999",
+        c : "Only 100",
+        d : "Any number",
+        ans : "d",
+        note : "We can define any number of classes with different names in a single program of OOPS."
+    },
+    {
+        no : 10,
+        q : "Which operator from the following can be used to illustrate the feature of polymorphism?",
+        a : "Overloading <<",
+        b : "Overloading &&",
+        c : "Overloading | |",
+        d : "Overloading +=",
+        ans : "a",
+        note : "<< is an insertion operator which is used for overloading (polymorphism)."
+    }
+];
+
+module.exports = Questions;
